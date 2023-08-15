afb35e62ff195f04355d1dd42c71a951f978d21f
Umairlari786 <uamir.lari1998@gmail.com>
2023-08-15 18:04:56 +0530

    added service page

--- src/routes/Service.js
+++ src/routes/Service.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import ServiceImg from "../assets/service-page.jpg"
+import Destination from '../components/Destination'
+import Footer from '../components/Footer'
+
 const Service = () => {
   return (
     <div>
@@ -14,6 +17,8 @@ const Service = () => {
     //  btn = "Show"
     //  buttonTxt = "Start Planning"
     />
+    <Destination />
+    <Footer />
     </div>
   )
 }
